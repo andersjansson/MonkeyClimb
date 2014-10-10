2b4b004afe0442636987ef4b2429d7f6c9b9e8f7
Josh Gomez <josh.gomez90@gmail.com>
2014-10-10 14:16:08 +0200

    PersistentData

---
+++ Assets/Scripts/Controllers/Main/PersistentDataController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Controllers.Main
+{
+	public class PersistentDataController : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		// Update is called once per frame
+		void Update () {
+		
+		}
+
+		void Awake()
+		{
+			DontDestroyOnLoad (this.gameObject);
+		}
+	}
+}
