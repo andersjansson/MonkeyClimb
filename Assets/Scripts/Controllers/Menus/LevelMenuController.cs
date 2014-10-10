53f0552d37d6927aed4b4a7701f7b9f178ce7841
Anders Jansson <nowevenbetter@gmail.com>
2014-10-10 14:10:09 +0200

    Added texture to LevelSelector button

--- Assets/Scripts/Controllers/Menus/LevelMenuController.cs
+++ Assets/Scripts/Controllers/Menus/LevelMenuController.cs
@@ -4,15 +4,31 @@ using System.Collections;
 namespace Controllers.Menus
 {
 	public class LevelMenuController : MonoBehaviour {
-
-		// Use this for initialization
-		void Start () {
 		
+		public Texture btnTexture;
+		
+		void OnGUI() {
+			float buttonWidth = Screen.width;
+			float buttonHeight = (Screen.height/3);
+			
+			Rect levelButton = new Rect(0,0,buttonWidth,buttonHeight);
+			
+			// Draw a button to start the game
+			if(GUI.Button(levelButton,"Jungle Level"))
+			{
+				Debug.Log("Clicked the button with an image");
+				//Application.LoadLevel("SinglePlayer");
+			}
+			/*
+			if (!btnTexture) {
+				Debug.LogError("Please assign a texture on the inspector");
+				return;
+			}*/
 		}
 		
 		// Update is called once per frame
 		void Update () {
-		
+			
 		}
 	}
 }
