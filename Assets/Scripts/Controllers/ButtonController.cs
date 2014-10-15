fb755ee7ad1996c13d1960c794f703a00447c671
Josh Gomez <josh.gomez90@gmail.com>
2014-10-15 15:51:26 +0200

    hej

---
+++ Assets/Scripts/Controllers/ButtonController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Controllers
+{
+	public class ButtonController : MonoBehaviour {
+
+		public string title = "Title";
+		public int imageWidth 	= 128;
+		public int imageHeight 	= 64;
+		public float fontSizeRatio = 10;
+
+		public GUIStyle style;
+
+		public Rect Button {get;set;}
+
+		// Use this for initialization
+		void Start () {
+		
+		}
+		
+		void OnGUI()
+		{
+			float width = this.imageWidth;
+			float height = this.imageHeight;
+
+			int textureHeight = style.normal.background.height;
+			int textureWidth = style.normal.background.width;
+
+			int screenHeight = Screen.height;
+			int screenWidth = Screen.width;
+			
+			int screenAspectRatio = (screenWidth / screenHeight);
+			int textureAspectRatio = (textureWidth / textureHeight);
+			
+			int scaledHeight;
+			int scaledWidth;
+			if (textureAspectRatio <= screenAspectRatio)
+			{
+				// The scaled size is based on the height
+				scaledHeight = screenHeight;
+				scaledWidth = (screenHeight * textureAspectRatio);
+			}
+			else
+			{
+				// The scaled size is based on the width
+				scaledWidth = screenWidth;
+				scaledHeight = (scaledWidth / textureAspectRatio);
+			}
+	
+
+			float x = this.transform.position.x * ((float)(Screen.width)  - scaledWidth);
+			float y = this.transform.position.y * ((float)(Screen.height) - scaledHeight);
+
+		
+			Button = new Rect(x,y,scaledWidth,scaledHeight);
+
+
+			float finalSize = Mathf.Min(width,height) / this.fontSizeRatio;
+			style.fontSize = (int)finalSize;
+		}
+	}
+}
--- Assets/Scripts/Controllers/Menus/MainMenuController.cs
+++ Assets/Scripts/Controllers/Menus/MainMenuController.cs
@@ -5,43 +5,31 @@ namespace Controllers.Menus
 {
 	public class MainMenuController : MonoBehaviour {
 
+		private GameObject buttonPlay;
+		private ButtonController buttonPlayController;
+
 		// Use this for initialization
 		void Start () {
 		
+			this.buttonPlay = GameObject.Find("ButtonPlay");
+			this.buttonPlayController = buttonPlay.GetComponent<ButtonController>();
 		}
 
 		void OnGUI()
 		{
-			const int buttonWidth = 84;
-			const int buttonHeight = 60;
-			
-			// Determine the button's place on screen
-			// Center in X, 2/3 of the height in Y
-			Rect buttonStart = new Rect(
-				Screen.width / 2 - (buttonWidth / 2),
-				(2 * Screen.height / 3) - (buttonHeight / 2),
-				buttonWidth,
-				buttonHeight
-				);
-			
-			Rect buttonQuit = new Rect(
-				Screen.width / 2 - (buttonWidth / 2),
-				(2 * Screen.height / 3) - (buttonHeight / 2) + buttonStart.height + 5,
-				buttonWidth,
-				buttonHeight
-				);
+
 			
 			// Draw a button to start the game
-			if(GUI.Button(buttonStart,"Play"))
+			if(GUI.Button(buttonPlayController.Button,buttonPlayController.title,buttonPlayController.style))
 			{
 				Application.LoadLevel("PlayMenu");
 			}
 			
 			// Draw a button to quit the game
-			if(GUI.Button(buttonQuit,"Quit"))
-			{
-				Application.Quit();
-			}
+			//if(GUI.Button(buttonQuit,"Quit"))
+			//{
+			//	Application.Quit();
+			//}
 		}
 		
 		// Update is called once per frame
