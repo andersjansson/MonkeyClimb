631e9e5a032a16509fa362dd05017e9e8883c2ed
Josh Gomez <josh.gomez90@gmail.com>
2014-10-15 19:57:58 +0200

    slask 2

---
+++ Assets/Scripts/TextureScaler.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Utilities;
+using Extensions;
+
+public class TextureScaler : MonoBehaviour {
+
+	public float orginalWidth;
+	public float orginalHeight;
+
+	void OnGUI()
+	{
+		var scaled = GUIScale.RectScale(orginalWidth,orginalHeight,this.transform.localScale);
+		guiTexture.pixelInset = new Rect(0, 0, scaled.Width, scaled.Height);
+	}
+}
