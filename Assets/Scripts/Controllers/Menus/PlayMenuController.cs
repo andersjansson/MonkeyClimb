6c39eabe675e93ff2d3942a7d5fa839cafc40c5a
Josh Gomez <josh.gomez90@gmail.com>
2014-10-17 13:49:21 +0200

    updated playmenu

--- Assets/Scripts/Controllers/Menus/PlayMenuController.cs
+++ Assets/Scripts/Controllers/Menus/PlayMenuController.cs
@@ -3,42 +3,33 @@ using System.Collections;
 
 namespace Controllers.Menus
 {
-	public class PlayMenuController : MonoBehaviour {
+	public class PlayMenuController : MonoBehaviour
+	{
+		private GameObject buttonSingleplayer;
+		private GameObject buttonBack;
 		
-		// Use this for initialization
-		void Start () {
+		private ButtonController buttonSingleplayerController;
+		private ButtonController buttonBackController;
+
+		void Start ()
+		{
+			this.buttonSingleplayer = GameObject.Find("ButtonSingleplayer");
+			this.buttonSingleplayerController = buttonSingleplayer.GetComponent<ButtonController>();
 			
+			this.buttonBack = GameObject.Find("ButtonBack");
+			this.buttonBackController = buttonBack.GetComponent<ButtonController>();
 		}
 		
 		void OnGUI()
 		{
-			const int buttonWidth = 84;
-			const int buttonHeight = 60;
-			
-			// Determine the button's place on screen
-			// Center in X, 2/3 of the height in Y
-			Rect buttonStart = new Rect(
-				Screen.width / 2 - (buttonWidth / 2),
-				(2 * Screen.height / 3) - (buttonHeight / 2),
-				buttonWidth,
-				buttonHeight
-				);
-			
-			Rect buttonQuit = new Rect(
-				Screen.width / 2 - (buttonWidth / 2),
-				(2 * Screen.height / 3) - (buttonHeight / 2) + buttonStart.height + 5,
-				buttonWidth,
-				buttonHeight
-				);
-			
 			// Draw a button to start the game
-			if(GUI.Button(buttonStart,"Singleplayer"))
+			if(GUI.Button(buttonSingleplayerController.Button,buttonSingleplayerController.title,buttonSingleplayerController.style))
 			{
 				Application.LoadLevel("LevelMenu");
 			}
 			
 			// Draw a button to quit the game
-			if(GUI.Button(buttonQuit,"Back"))
+			if(GUI.Button(buttonBackController.Button,buttonBackController.title,buttonBackController.style))
 			{
 				Application.LoadLevel("MainMenu");
 			}
