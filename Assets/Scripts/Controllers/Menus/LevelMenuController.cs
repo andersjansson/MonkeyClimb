f18907e817b82745d10a054829d7e94fa15c2d21
Anders Jansson <nowevenbetter@gmail.com>
2014-10-17 14:49:32 +0200

    wat

--- Assets/Scripts/Controllers/Menus/LevelMenuController.cs
+++ Assets/Scripts/Controllers/Menus/LevelMenuController.cs
@@ -6,35 +6,42 @@ namespace Controllers.Menus
 	public class LevelMenuController : MonoBehaviour {
 
 		private GameObject buttonJungle;
+		private GameObject buttonBack;
 
 		private ButtonController buttonJungleController;
+		private ButtonController buttonBackController;
 
 		void Start(){
-			this.buttonJungle = GameObject.Find("ButtonJungle");
+			this.buttonJungle 	= GameObject.Find("ButtonJungle");
+			this.buttonBack 	= GameObject.Find("ButtonBack");
+
 			this.buttonJungleController = buttonJungle.GetComponent<ButtonController>();
+			this.buttonBackController 	= buttonBack.GetComponent<ButtonController>();
 		}
 
 		void OnGUI() {
-			float buttonWidth = Screen.width;
-			float buttonHeight = (Screen.height/4);
-			
-			Rect levelButton = new Rect(0,100,buttonWidth,buttonHeight);
-			
+
 			// Draw a button to start the game
 			if(GUI.Button(buttonJungleController.Button,buttonJungleController.title,buttonJungleController.style))
 			{
+				Application.LoadLevel("Jungle");
+			}
+			// Draw a button to go back
+			if(GUI.Button(buttonBackController.Button,buttonBackController.title,buttonBackController.style))
+			{
 				Application.LoadLevel("PlayMenu");
 			}
-			/*
-			if (!btnTexture) {
-				Debug.LogError("Please assign a texture on the inspector");
-				return;
-			}*/
 		}
 		
 		// Update is called once per frame
 		void Update () {
-			
+
+			//Android back-button
+			if (Input.GetKey (KeyCode.Escape))
+			{
+				Application.LoadLevel("PlayMenu");
+			}
+
 		}
 	}
 }
