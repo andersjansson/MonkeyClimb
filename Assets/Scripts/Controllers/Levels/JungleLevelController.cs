07d149114553439317c57b2f0a17b56aa9d2a92c
Josh Gomez <josh.gomez90@gmail.com>
2014-10-25 12:31:41 +0200

    new distance clouds.

--- Assets/Scripts/Controllers/Levels/JungleLevelController.cs
+++ Assets/Scripts/Controllers/Levels/JungleLevelController.cs
@@ -7,13 +7,34 @@ namespace Controllers.Level
 {
 	public class JungleLevelController : MonoBehaviour
 	{
+		private GameObject skyboxPart1;
+		private bool cloudFarSpawned = false;
+
 		void Start ()
 		{
 			GameController.SetLevelCollider ("Level/Middlegrounds/Middlegrounds - Main/TreeTrunk1");
 			AudioController.Play ("ButtonSound",5,forward:this.StartSound);
+
+			this.skyboxPart1 = GameObject.Find("Level/Skyboxes/SkyboxJunglePart1");
 		}
 
-		void StartSound()
+		void Update ()
+		{
+			if(!this.cloudFarSpawned && this.skyboxPart1 != null)
+			{
+				if(this.skyboxPart1.transform.position.y + this.skyboxPart1.renderer.bounds.size.y/2f  < Camera.main.transform.position.y)
+				{
+					GameObject cloudSpawner = GameObject.Find("Scroller/SpawnerClouds - Far");
+					if(cloudSpawner != null)
+					{
+						cloudSpawner.GetComponent<SpawnController>().enabled = true;
+						this.cloudFarSpawned = true;
+					}
+				}
+			}
+		}
+
+		private void StartSound()
 		{
 			AudioController.Play("BackgroundSound",2);
 		}
