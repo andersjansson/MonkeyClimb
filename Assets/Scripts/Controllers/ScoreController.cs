fd92fc57203e02fdaaf7331e09cace7d51babba1
Josh Gomez <josh.gomez90@gmail.com>
2014-11-07 15:23:08 +0100

    test collision2

---
+++ Assets/Scripts/Controllers/ScoreController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+namespace Controllers
+{
+	public class ScoreController : MonoBehaviour
+	{
+		public GameObject labelObject;
+		private LabelController label;
+
+		private float points = 0.12345123f;
+		public float Points
+		{
+			get
+			{
+				return points;
+			}
+			set
+			{
+				this.points = value;
+				this.UpdateScore();
+			}
+		}
+
+		// Use this for initialization
+		void Start ()
+		{
+			if(this.labelObject != null)
+			{
+				this.label = this.labelObject.GetComponent<LabelController>();
+				this.UpdateScore();
+			}
+		}
+
+		private void UpdateScore()
+		{
+			this.label.title = String.Format ("Distance: {0} Meters", this.Points.ToString("F1"));
+		}
+	}
+}
